b198d954f3fbbd936bb6ebf90a43bb3648858ac8
JohnN05 <johnng0511@gmail.com>
2024-07-29 22:07:20 -0400

    Minor changes to Navigation section

--- src/Footer.js
+++ src/Footer.js
@@ -1,38 +1,40 @@
 import logo from './assets/Logo.svg'
 function Footer(){
-    <nav>
-        <img alt='Little Lemon Logo' src={logo}></img>
-        <section>
-            <h3>Navigation</h3>
-            <ul>
-                <li><a href=''></a></li>
-                <li><a href=''></a></li>
-                <li><a href=''></a></li>
-                <li><a href=''></a></li>
-                <li><a href=''></a></li>
-                <li><a href=''></a></li>
-            </ul>
-        </section>
-        <section>
-            <address>
-                <h3>Contact</h3>
+    return(
+        <nav>
+            <img alt='Little Lemon Logo' src={logo}></img>
+            <section>
+                <h3>Navigation</h3>
                 <ul>
-                    <li><a href=''>Address</a></li>
-                    <li><a href=''>Phone Number</a></li>
-                    <li><a href=''>Email</a></li>
+                    <li><a href=''>Home</a></li>
+                    <li><a href=''>About</a></li>
+                    <li><a href=''>Menu</a></li>
+                    <li><a href=''>Reservations</a></li>
+                    <li><a href=''>Order Online</a></li>
+                    <li><a href=''>Login</a></li>
                 </ul>
-            </address>
+            </section>
+            <section>
+                <address>
+                    <h3>Contact</h3>
+                    <ul>
+                        <li><a href=''>Address</a></li>
+                        <li><a href=''>Phone Number</a></li>
+                        <li><a href=''>Email</a></li>
+                    </ul>
+                </address>
 
+            </section>
+            <section>
+                <h3>Social Media</h3>
+                <ul>
+                    <li><a href=''></a>Facebook</li>
+                    <li><a href=''></a>Instagram</li>
+                    <li><a href=''></a>Pinterest</li>
+                </ul>
         </section>
-        <section>
-            <h3>Social Media</h3>
-            <ul>
-                <li><a href=''></a>Facebook</li>
-                <li><a href=''></a>Instagram</li>
-                <li><a href=''></a>Pinterest</li>
-            </ul>
-        </section>
-    </nav>
+        </nav>
+    );
 }
 
 export default Footer;
