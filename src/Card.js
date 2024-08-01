9ae764dce2e732e499094b003c027eab5fb7eaf3
JohnN05 <johnng0511@gmail.com>
2024-08-01 12:49:44 -0400

    Finalize Card component

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,16 @@
+import delivery from './assets/fast-delivery.png';
+
 function Card(props){
     return(
     <section className="Card">
+        <img className="CardImage" alt={props.name} src={props.image}></img>
+        <h3>{props.name}</h3>
+        <h4>{props.price}</h4>
+        <p>{props.description}</p>
+        <div>
+            <h5>Order a delivery </h5>
+            <img alt="Food delivery icon" height="25px" src={delivery}></img> 
+        </div>
         
     </section>)
 }
