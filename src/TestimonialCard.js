5b0479984050f27801d693aaa61fea67a896da70
JohnN05 <johnng0511@gmail.com>
2024-08-01 15:56:14 -0400

    Finish Testimonial Section

--- src/TestimonialCard.js
+++ src/TestimonialCard.js
@@ -1,4 +1,15 @@
-function TestimonialCard(){
+import Rating from "./Rating";
+function TestimonialCard(props){
+    const imageCaption=`Picture of ${props.name}`;
+    return(
+        <article className="Testimonial">
+            <h3>Rating</h3>
+            <Rating count={props.rating} />
+            <img alt={imageCaption} src={props.image}></img>
+            <h3 className="TestimonialName">{props.name}</h3>
+            <p>"{props.quote}"</p>
 
+        </article>
+    );
 }
 export default TestimonialCard;
--- src/Testimonials.js
+++ src/Testimonials.js
@@ -1,32 +1,31 @@
 import TestimonialCard from "./TestimonialCard";
+import p2 from "./assets/p2.jpg";
+import p3 from "./assets/p3.jpg";
+import p4 from "./assets/p4.jpg";
+
 function Testimonials(){
     return(
-        <section>
+        <section className="TestimonialsSection">
             <h1>Testimonials</h1>
             <div className="Testimonials">
-                <TestimonialCard 
-                rating="5" 
-                image="" 
-                name="John Smith" 
-                quote=""/>
-                
-                <TestimonialCard 
-                rating="5" 
-                image="" 
-                name="John Smith" 
-                quote=""/>
 
                 <TestimonialCard 
-                rating="5" 
-                image="" 
-                name="John Smith" 
-                quote=""/>
+                rating={4} 
+                image={p2}
+                name="Michael T." 
+                quote="I can't get enough of Little Lemon's delicious gyros. The quality and taste are unmatched, and the service is always friendly and welcoming."/>
 
                 <TestimonialCard 
-                rating="5" 
-                image="" 
-                name="John Smith" 
-                quote=""/>
+                rating={5} 
+                image={p3}
+                name="Liam W." 
+                quote="Little Lemon's vibrant and cozy atmosphere, paired with their authentic dishes, make it my favorite spot for a Mediterranean meal. The grilled kebabs are a must-try!"/>
+
+                <TestimonialCard 
+                rating={4.5} 
+                image={p4}
+                name="Sophia L." 
+                quote="Little Lemon has quickly become my go-to restaurant for Mediterranean cuisine. The flavors are bold and authentic, and the staff treats you like family."/>
             </div>
         </section>
     )
