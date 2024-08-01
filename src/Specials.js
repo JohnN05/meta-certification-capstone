bc3b097179492a293d8351c34e30609eeb189b13
JohnN05 <johnng0511@gmail.com>
2024-08-01 13:13:47 -0400

    Finish styling Specials component

--- src/Specials.js
+++ src/Specials.js
@@ -11,27 +11,26 @@ function Specials(){
                 <h1>This weeks specials!</h1>
                 <Link to="/"><button type="button"><h3>Online Menu</h3></button></Link>
             </div>
-            <ul>
-                <li><Card 
+            <div className="Cards">
+                <Card 
                 description="The famous greek salad of crispy lettuce, peppers, olives, and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons."  
                 image={greekSalad}
                 name="Greek Salad" 
                 price="$9.99"/>
-                </li>
-                <li><Card 
+                
+                <Card 
                 description="Our bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil."  
                 image={bruschetta}
                 name="Bruschetta" 
                 price="$12.99"/>
-                </li>
-                <li><Card 
+                
+                <Card 
                 description="This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined."  
                 image={lemonDessert}
                 name="Lemon Dessert" 
                 price="$5.49"/>
-                </li>
-
-            </ul>
+            
+            </div>
         </section>
     );
 }
