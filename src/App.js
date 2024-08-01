f7e9984a263d79f1db12858c80f99f46ea254de6
JohnN05 <johnng0511@gmail.com>
2024-08-01 00:57:46 -0400

    Move all react components to Main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import Header from './Header';
-import Main from './Main'
-import Footer from './Footer';
+import Main from "./Main";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
+    <Main />
+    
   );
 }
 
