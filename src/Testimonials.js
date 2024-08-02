2a09eb3b90ec1dc1c7a5401e4668dbeb5ad2085a
JohnN05 <johnng0511@gmail.com>
2024-08-02 00:45:33 -0400

    Make Testimonials, CallToAction, Specials, and AboutUs more responsive for smaller devices

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -6,27 +6,31 @@ import p4 from "./assets/p4.jpg";
 function Testimonials(){
     return(
         <section className="TestimonialsSection">
-            <h1>Testimonials</h1>
-            <div className="Testimonials">
+            <div className="Container">
+                <h1>Testimonials</h1>
+                <div className="Testimonials">
 
-                <TestimonialCard 
-                rating={4} 
-                image={p2}
-                name="Michael T." 
-                quote="I can't get enough of Little Lemon's delicious gyros. The quality and taste are unmatched, and the service is always friendly and welcoming."/>
+                    <TestimonialCard 
+                    rating={4} 
+                    image={p2}
+                    name="Michael T." 
+                    quote="I can't get enough of Little Lemon's delicious gyros. The quality and taste are unmatched, and the service is always friendly and welcoming."/>
 
-                <TestimonialCard 
-                rating={5} 
-                image={p3}
-                name="Liam W." 
-                quote="Little Lemon's vibrant and cozy atmosphere, paired with their authentic dishes, make it my favorite spot for a Mediterranean meal. The grilled kebabs are a must-try!"/>
+                    <TestimonialCard 
+                    rating={5} 
+                    image={p3}
+                    name="Liam W." 
+                    quote="Little Lemon's vibrant and cozy atmosphere, paired with their authentic dishes, make it my favorite spot for a Mediterranean meal. The grilled kebabs are a must-try!"/>
 
-                <TestimonialCard 
-                rating={4.5} 
-                image={p4}
-                name="Sophia L." 
-                quote="Little Lemon has quickly become my go-to restaurant for Mediterranean cuisine. The flavors are bold and authentic, and the staff treats you like family."/>
+                    <TestimonialCard 
+                    rating={4.5} 
+                    image={p4}
+                    name="Sophia L." 
+                    quote="Little Lemon has quickly become my go-to restaurant for Mediterranean cuisine. The flavors are bold and authentic, and the staff treats you like family."/>
+                </div>
             </div>
+            
+            
         </section>
     )
 }
