34faacf3cc447032e19a7a93e1d60d06e972301c
JohnN05 <johnng0511@gmail.com>
2024-08-03 14:03:45 -0400

    Create FakeAPI to emulate API behavior in original assignment

---
+++ src/FakeAPI.js
@@ -0,0 +1,23 @@
+const seededGenerator = (date, hour) => {
+    const m = 9;
+    const result = ((date*4 + hour) % m ) / 10;
+  
+    return result;
+}
+
+export const fetchAPI = function(date) {
+    let result = [];
+
+    for(let i = 17; i <= 23; i++) {
+        if(seededGenerator(date, i) < 0.5) {
+            result.push(i + ':00');
+        }
+        if(seededGenerator(date, i+7) < 0.5) {
+            result.push(i + ':30');
+        }
+    }
+    return result;
+};
+export const submitAPI = function(formData) {
+    return true;
+};
