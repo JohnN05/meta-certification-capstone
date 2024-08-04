24eaa5fed6785e54554573007846b68ee7ac3336
JohnN05 <johnng0511@gmail.com>
2024-08-04 14:44:00 -0400

    Add validation for time, date, and occasion fields

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,5 +1,5 @@
 
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 
 function BookingForm(props){
@@ -12,8 +12,20 @@ function BookingForm(props){
         backgroundColor: resOccasion=="occasion" ? '#edefee' : '#495e57',
         color: resOccasion=="occasion" ? '#494949' : 'white'
     }
-    
+
+    const [valid, setValid] = useState({date: false, time: false, occasion: false})
+    const [disabledButton, setDisabledButton] = useState(true);
+
+    const [dateError, setDateError] = useState(false);
+    const [timeError, setTimeError] = useState(false);
+    const [occasionError, setOccasionError] = useState(false);
+
+    useEffect(() => {
+        setDisabledButton(!(valid.date && valid.occasion && valid.time));
+    }, [valid]);
+
     const navigate = useNavigate();
+
 
     return(
         
@@ -26,9 +38,6 @@ function BookingForm(props){
             guests: {resGuests},
             occasion: {resOccasion}
             });
-            
-            
-                
             
             if(success){
                 setTimeout(() => {
@@ -43,14 +52,23 @@ function BookingForm(props){
                 id="res-date" 
                 value={resDate}
                 onChange={(e)=>{
-                    setResDate(e.target.value)
+                    setResDate(e.target.value);
+                    const validDate = e.target.value!="";
+                    setValid({...valid, date:validDate});
+                    setDateError(!validDate);
                     props.dispatch({type: e.target.value})
                 }}
                 />
+                {dateError && <p>Please choose a date for your reservation</p>}
                 <label htmlFor="res-time">Choose time</label>
                 <select id="res-time" 
                 value={resTime}
-                onChange={(e)=>{setResTime(e.target.value)}}>
+                onChange={(e)=>{
+                    setResTime(e.target.value);
+                    const validTime = e.target.value!="";
+                    setValid({...valid, time:validTime});
+                    setTimeError(!validTime);
+                    }}>
                     {
                     props.availableTimes.map((timeOption)=>{
                         return(
@@ -58,6 +76,7 @@ function BookingForm(props){
                         );
                     })}
                 </select>
+                {timeError && <p>Please choose a time for your reservation</p>}
                 <label htmlFor="guests">Number of guests: {resGuests}</label>
                 <input type="range" 
                 aria-valuemin={1}
@@ -67,14 +86,17 @@ function BookingForm(props){
                 max="10" 
                 id="guests" 
                 value={resGuests}
-                onChange={(e)=>{setResGuests(e.target.value)}
+                onChange={(e)=>{setResGuests(e.target.value);}
                 }
                 />
                 <label htmlFor="occasion">Occasion</label>
                 <select id="occasion" 
                 value={resOccasion}
                 onChange={(e)=>{
-                    setResOccasion(e.target.value)
+                    setResOccasion(e.target.value);
+                    const validOccasion = e.target.value!="occasion";
+                    setValid({...valid, occasion:validOccasion});
+                    setOccasionError(!validOccasion);
                 }}
                 style={occasionStyle}
                 >
@@ -83,9 +105,12 @@ function BookingForm(props){
                     <option value="engagement" style={occasionStyle}>Engagement</option>
                     <option value="anniversary" style={occasionStyle}>Anniversary</option>
                 </select>
+                {occasionError && <p>Please choose an occasion for your reservation</p>}
             </fieldset>
-            <input className="Button" type="submit" 
+            <input className="Button" 
+            type="submit" 
             value="Make your reservation" 
+            disabled={disabledButton}
             />
         </form>
     );
