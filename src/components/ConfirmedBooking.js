d6cc43439857f0d1d4e3f4af9a76bf895d6bc7bf
JohnN05 <johnng0511@gmail.com>
2024-08-04 12:30:36 -0400

    Finish styling booking confirmation page

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -1,7 +1,11 @@
+import chef from '../assets/restaurant chef B.jpg'
 function ConfirmedBooking(){
     return(
         <section className="ConfirmedBooking">
             <h1>Booking has been confirmed</h1>
+            <h2>See you soon!</h2>
+            <img alt="Picture of a dish from Little Lemon" src={chef}></img>
+
         </section>
     );
 }
